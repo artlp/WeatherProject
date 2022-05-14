2b72a4197900fed56bc9afadc87c54d12a4cdabd
Artur L <falcon.arthur@gmail.com>
2022-05-14 14:51:32 +0300

    1.2

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 const port = 3000;
 const https = require("https");
+const url = require('url');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.get('/', (req, res) => res.send('Hello World!'))
 app.get("/", function (req, res) {
-    res.send("server is live");
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Odintsovo&appid=bf4571a67aa57ec82fdd16233046cc8f&units=metric";
-    https.get(url, function (response) {
-        // console.log(response.statusCode);
-        response.on("data", function(data){
-            const weatherData = JSON.parse(data);
-            // console.log(weatherData);
-            const temp = weatherData.main.temp;
-            const weatherDesc = weatherData.weather[0].description;
-            console.log("Current temperature is " + temp + ". " + weatherDesc);
-        })
+    res.sendFile(__dirname + "/index.html");
+    app.post("/", function (req, res) {
+        const query = req.body.cityName;
+        const appid = "bf4571a67aa57ec82fdd16233046cc8f";
+        const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + appid + "&units=metric";
+        https.get(url, function (response) {
+            // console.log(response.statusCode);
+            response.on("data", function (data) {
+                const weatherData = JSON.parse(data);
+                // console.log(weatherData);
+                const temp = weatherData.main.temp;
+                const weatherDesc = weatherData.weather[0].description;
+                const icon = weatherData.weather[0].icon;
+                const newUrl = new URL('https://openweathermap.org/img/wn/' + icon + '.png');
+                console.log(newUrl.pathname);
+                res.write("<h1> Welcome to the new generation of weather service</h1>");
+                res.write("<h2>Current temperature in " + query + " is " + temp + ". The weather is currently " + weatherDesc + ".</h2>");
+                res.write("<img src=" + `${newUrl}` + ">");
+                res.end();
+            });
+        });
+            });
+        });
 
-    });
+        // });
 
-});
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+        // });
+        app.listen(port, () => console.log(`Example app listening on port ${port}!`))
